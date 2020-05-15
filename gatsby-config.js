0c3e442c958e43f64559e24f2fcdc4fc54798d07
Malte <malte.thomsen@live.de>
2020-05-16 00:05:10 +0200

    Title Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Impugnaris Blog',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Blog für Forza Horizon 4. Fahrzeugreviews, Tests, Tuning, Designs.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
